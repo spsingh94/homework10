17f4e2ad6c0ad25865d017eece34c649a4004b73
Shaminder Singh <spsingh94.ss@gmail.com>
2020-05-10 15:00:07 -0400

    updated functions to add to database

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -1,7 +1,50 @@
 const db = require("./query_file/employee");
 const inquirer = require("inquirer");
 
+//add func
 async function addEmployee() {
+  var roles = await db.getRoles();
+  // console.log(roles);
+  var employee = inquirer.prompt([{
+    type: "input",
+    name: "firstName",
+    message: "What is the employees first name?"
+  }, {
+    type: "input",
+    name: "lastName",
+    message: "What is the employees last name?"
+  }, {
+    type: "list",
+    name: "role",
+    message: "What is the employees role?",
+    choices: [
+      { name: "Marketing", value: "1" },
+      { name: "Sales", value: "2" },
+      { name: "HR", value: "3" },
+      { name: "Accounting", value: "4" }
+    ]
+  }
+  ]).then(function (res) {
+    insertEmployee(res);
+  });
+};
+function insertEmployee(data) {
+  db.connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      first_name: data.firstName,
+      last_name: data.lastName,
+      role_id: data.role
+    },
+    function (err, res) {
+      if (err) throw err;
+    }
+  )
+}
+//add func
+
+// view func
+async function viewEmployee() {
   var roles = await db.getRoles();
   console.log(roles);
   var employee = inquirer.prompt([{
@@ -16,27 +59,65 @@ async function addEmployee() {
     type: "list",
     name: "role",
     message: "What is the employees role?",
-    choices: [{ name: "Marketing", value: "1" }, { name: "Sales", value: "2" },
-    { name: "HR", value: "3" }, { name: "Accounting", value: "4" }]
+    choices: [
+      { name: "Marketing", value: "1" },
+      { name: "Sales", value: "2" },
+      { name: "HR", value: "3" },
+      { name: "Accounting", value: "4" }
+    ]
   }
   ])
 }
+// view func
 
+// update func
+async function viewEmployee() {
+  var roles = await db.getRoles();
+  console.log(roles);
+  var employee = inquirer.prompt([{
+    type: "input",
+    name: "firstName",
+    message: "What is the employees first name?"
+  }, {
+    type: "input",
+    name: "lastName",
+    message: "What is the employees last name?"
+  }, {
+    type: "list",
+    name: "role",
+    message: "What is the employees role?",
+    choices: [
+      { name: "Marketing", value: "1" },
+      { name: "Sales", value: "2" },
+      { name: "HR", value: "3" },
+      { name: "Accounting", value: "4" }
+    ]
+  }
+  ])
+}
+//update func
+
+//initiating func
 function init() {
   inquirer.prompt([{
     type: "list",
     message: "What would you like to do?",
     name: "value",
-    choices: [{ name: "Add an employee", value: "addEmployee" }, { name: "Add an employee", value: "addEmployee" },
-    { name: "Add an employee", value: "addEmployee" }, { name: "Add an employee", value: "addEmployee" },
-    { name: "Add an employee", value: "addEmployee" },{ name: "Add an employee", value: "addEmployee" }]
+    choices: [
+      { name: "Add an employee", value: "addEmployee" },
+      { name: "View all employees", value: "viewEmployee" },
+      { name: "Update an employee role", value: "updateEmployee" }
+    ]
   }]).then(function (choices) {
-    //   if (choices){
-    //     return (addEmployee())
-    // }
     switch (choices.value) {
       case "addEmployee":
         addEmployee();
+        break;
+      case "viewEmployee":
+        viewEmployee();
+        break;
+      case "updateEmployee":
+        updateEmployee();
         break;
       default:
         console.log("that was not an option.")
